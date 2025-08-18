f7bab98092e7fd2f4761a62af2916a68ce7cae09
srdsoproject <srdsosurproject@gmail.com>
2025-08-18 16:41:09 +0530

    Update drive.py

--- drive.py
+++ drive.py
@@ -5,7 +5,7 @@ url = st.secrets["onedrive"]["url"]
 
 try:
     df = pd.read_excel(url)
-    st.success("✅ Loaded successfully from OneDrive")
+    st.success("✅ File loaded successfully from OneDrive")
     st.dataframe(df)
 except Exception as e:
-    st.error(f"❌ Could not load from OneDrive: {e}")
+    st.error(f"❌ Could not load file: {e}")
